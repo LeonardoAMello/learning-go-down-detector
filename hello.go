4adfc33125840b3e54cc1c3c87fe78502921b9fd
Leonardo Mello <leonardodeabreumello@gmail.com>
2024-06-08 15:20:24 -0300

    Sending GET request to monitor famous websites

--- hello.go
+++ hello.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"strconv"
 )
 
 func printMenu() {
@@ -34,7 +36,42 @@ func printMenu() {
 }
 
 func startAnalisys() {
+	sites := []string{
+		"https://www.google.com",
+		"https://www.youtube.com",
+		"https://www.facebook.com",
+		"https://www.amazon.com",
+		"https://www.wikipedia.org",
+		"https://www.twitter.com",
+		"https://www.instagram.com",
+		"https://www.linkedin.com",
+		"https://www.netflix.com",
+		"https://www.reddit.com",
+		"https://www.yahoo.com",
+		"https://www.ebay.com",
+		"https://www.bing.com",
+		"https://www.msn.com",
+		"https://www.pinterest.com",
+		"https://www.tumblr.com",
+		"https://www.paypal.com",
+		"https://www.espn.com",
+		"https://www.bbc.com",
+		"https://www.cnn.com",
+	}
 
+	for _, site := range sites {
+		fmt.Println(checkSite(site))
+	}
+}
+
+func checkSite(site string) string {
+	response, err := http.Get(site)
+
+	if err != nil {
+		return site + " NOK"
+	}
+
+	return site + " " + strconv.Itoa(response.StatusCode)
 }
 
 func showHistory() {
